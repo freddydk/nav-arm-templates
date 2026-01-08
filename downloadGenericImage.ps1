3e42f8af875f35054695056d51e46ad8f37afe4f
Freddy Kristiansen <fkr@freddy.dk>
2026-01-08 12:51:27 +0100

    replace string

--- downloadGenericImage.ps1
+++ downloadGenericImage.ps1
@@ -12,3 +12,19 @@ Copy-Item -Path $runPath -Destination "C:\" -force -Recurse
 Remove-Item -Path $zipPath -Force
 Remove-Item $tempDir -Recurse -Force
 Get-ChildItem -Path "c:\run" | Out-Host
+
+function ReplaceStr {
+    param (
+        [string] $FileName,
+        [string] $SearchStr,
+        [string] $ReplaceStr
+    )
+    Set-Content -Path $FileName -Encoding utf8 -Value (Get-Content -Path $FileName -raw -Encoding utf8).Replace($SearchStr, $ReplaceStr)
+}
+
+ReplaceStr -FileName: 'c:/run/SetupUrls.ps1' -SearchStr: 'https://aka.ms/bcdocker-Sql2022Url' -ReplaceStr: 'https://aka.ms/sqlserver2022developer'
+ReplaceStr -FileName: 'c:/run/SQLConf.ini' -SearchStr: 'SQLMAXMEMORY="2147483647"' -ReplaceStr: 'SQLMAXMEMORY="22528"'
+ReplaceStr -FileName: 'c:/run/SQLConf.ini' -SearchStr: '"Manual"' -ReplaceStr: '"Automatic"'
+ReplaceStr -FileName: 'c:/run/SetupGeneric1.ps1' -SearchStr: 'SQLEXPRADV_x64_ENU.exe' -ReplaceStr: 'SQLServer2022-DEV-x64-ENU.exe'
+ReplaceStr -FileName: 'c:/run/SetupGeneric1.ps1' -SearchStr: ', "/MediaType=Advanced"' -ReplaceStr: ''
+ReplaceStr -FileName: 'c:/run/HelperFunctions.ps1' -SearchStr: '[string] $basePath = ''c:\dl''' -ReplaceStr: '[string] $basePath = ''c:\bcartifacts.cache'''
